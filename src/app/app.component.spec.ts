a137a118a1c4f9469a70183bce9a4c099a29c6ea
MarMariam <m.arakelyan@omedia.ge>
2024-10-31 16:14:42 +0100

    initial pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'photo-library-by-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('photo-library-by-angular');
   });
 
   it('should render title', () => {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: GalleryComponent,
+    },
+    {
+        path: 'favorites',
+        component: FavoritesComponent,
+    },
+    {
+      path: '**',
+      component: PageNotFoundComponent,
+    },
+];
