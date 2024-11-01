c79f67c2252710e1ed42944bf4e54e774f5c1e59
MarMariam <m.arakelyan@omedia.ge>
2024-11-01 14:34:03 +0100

    testes for services

--- src/app/core/services/dbmain.service.spec.ts
+++ src/app/core/services/dbmain.service.spec.ts
@@ -1,10 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 
 import { DBMainService } from './dbmain.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { PHOTOS } from '../../../../test-data/photos';
+import { BehaviorSubject } from 'rxjs';
+import { Photo } from '../interfaces/photo.interface';
 
 describe('DBMainService', () => {
   let service: DBMainService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,10 +16,59 @@ describe('DBMainService', () => {
         HttpClientTestingModule,
       ]
     });
+
     service = TestBed.inject(DBMainService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should retrieve all photos from picsum open API', () => {
+    service.load().subscribe((photos) => {
+      expect(photos).toBeTruthy('No photos returned');
+
+      expect(photos.length).toBe(39, 'incorrect number of photos');
+
+      const photo = photos.find((photo) => photo.id === 10);
+
+      expect(photo?.author).toBe('Paul Jarvis');
+    });
+
+    const req = httpTestingController.expectOne('https://picsum.photos/list');
+
+    expect(req.request.method).toEqual('GET');
+
+    // Pass test data on our httpRequest
+    req.flush(PHOTOS);
+  });
+
+  it('should retrieve a range of photos from lower, upper arguments', fakeAsync(() => {
+    service['fillDB'] = new BehaviorSubject<Photo[]>(PHOTOS);
+    service['EmulatePhotosDB$'] =
+    service['fillDB'].asObservable();
+
+    service.getRange(2, 10).subscribe((photos) => {
+      expect(photos).toBeTruthy('No photos returned');
+
+      expect(photos.length).toBe(10, 'incorrect number of photos returned');
+
+      const photo = photos.find((photo) => photo.id == 1016);
+
+      expect(photo?.author).toBe('Philippe Wuyts');
+    });
+
+    tick(700);
+
+    flush();
+  }));
+
+  it('check if db data is empty', () => {
+    service['fillDB'] = new BehaviorSubject<Photo[]>(PHOTOS);
+
+    let isEmpty = service.isEmpty();
+    expect(isEmpty).toBe(false, 'Expected non empty database');
+
+    service['fillDB'] = new BehaviorSubject<Photo[]>([]);
+
+    isEmpty = service.isEmpty();
+    expect(isEmpty).toBe(true, 'Expected non empty database');
   });
 });
--- src/app/core/services/photos.service.spec.ts
+++ src/app/core/services/photos.service.spec.ts
@@ -2,18 +2,66 @@ import { TestBed } from '@angular/core/testing';
 
 import { PhotosService } from './photos.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DBMainService } from './dbmain.service';
+import { Photo } from '../interfaces/photo.interface';
+import { PHOTOS } from '../../../../test-data/photos';
+import { FAVORITES } from '../../../../test-data/favorites';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 describe('PhotosService', () => {
   let service: PhotosService;
+  let mainDbService: any;
 
   beforeEach(() => {
+    mainDbService = jasmine.createSpyObj('DBMainService', ['getRange', 'load']);
+
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [
+        PhotosService,
+        { provide: DBMainService, useValue: mainDbService },
+      ]
     });
+
     service = TestBed.inject(PhotosService);
+    service['updatePhotos'] = new BehaviorSubject<Photo[]>(PHOTOS);
+    service['saveState'] = new BehaviorSubject<Photo[]>(FAVORITES);
+    service['state'] = service['saveState'] as Observable<Photo[]>;
+    service['totalPhotos'] = 100;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add to favorite', () => {
+    const selectFirst = PHOTOS[0];
+
+    service.addToFavorite(PHOTOS[0]);
+    const state = service['state'].subscribe((photos) => {
+      const favorite = photos.find((p) => p.id === selectFirst.id);
+
+      expect(favorite?.favorite).toBe(true, 'Favorite status not changed');
+    });
+  });
+
+  it('should remove from favorite', () => {
+    const selectFirst = FAVORITES[0];
+
+    service.removeFromFavorite(selectFirst);
+    const state = service['state'].subscribe((photos) => {
+      const favorite = photos.find((p) => p.id === selectFirst.id);
+
+      expect(favorite).toBe(undefined, 'Favorite status not changed');
+    });
+  });
+
+  it('check favorite status of list after refresh', () => {
+    const updatedPhotos = service.getFavoriteStatus(PHOTOS);
+
+    expect(updatedPhotos[0].favorite).toBe(
+      true,
+      'Favorite status not updated after refresh'
+    );
   });
 });
